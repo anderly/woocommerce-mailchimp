90c706059f0f06b1c3777f694fe85f90339e254a
Risto Niinemets <risto.niinemets@gmail.com>
2015-02-06 15:05:54 +0200

    update pot file

--- classes/class-ss-wc-integration-mailchimp.php
+++ classes/class-ss-wc-integration-mailchimp.php
@@ -376,7 +376,7 @@ class SS_WC_Integration_MailChimp extends WC_Integration {
 	public function mailchimp_api_error_msg() {
 		echo $this->get_message(
 			sprintf( __( 'Unable to load lists from MailChimp: (%s) %s. ', 'ss_wc_mailchimp' ), $this->mailchimp->errorCode, $this->mailchimp->errorMessage ) .
-			sprintf( __( 'Please check your <a href="' . WOOCOMMERCE_MAILCHIMP_SETTINGS_URL . '">settings</a>', 'ss_wc_mailchimp' ) )
+			sprintf( __( 'Please check your %s <a href="%s">settings</a>.', 'ss_wc_mailchimp' ), WOOCOMMERCE_MAILCHIMP_SETTINGS_URL )
 		);
 	}
 
