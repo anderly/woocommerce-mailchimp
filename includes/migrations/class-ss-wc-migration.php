5c5608879035a78e5e104425b8dc7f75b1817268
Adam Anderly <adam@anderly.com>
2016-09-15 16:00:22 -0500

    Added logic to support 1.3.X and 2.X settings.

--- includes/migrations/class-ss-wc-migration.php
+++ includes/migrations/class-ss-wc-migration.php
@@ -6,9 +6,32 @@ abstract class SS_WC_MailChimp_Migration_Base {
 
 	protected $settings;
 
+	protected $namespace = 'ss_wc_mailchimp';
+
+	protected $current_version;
+	protected $target_version;
+
+	protected $setting_keys = array(
+		'api_key',
+		'enabled',
+		'list',
+		'interest_groups',
+		'display_opt_in',
+		'occurs',
+		'double_opt_in',
+		'opt_in_label',
+		'opt_in_checkbox_default_status',
+		'opt_in_checkbox_display_location',
+	);
+
 	protected $settings_key = 'woocommerce_mailchimp_settings';
 
-	public function __construct() {
+	public function __construct( $current_version, $target_version ) {
+
+		$this->settings = array();
+
+		$this->current_version = $current_version;
+		$this->target_version = $target_version;
 
 		if ( ! $this->load_settings() ) return;
 
@@ -24,12 +47,22 @@ abstract class SS_WC_MailChimp_Migration_Base {
 	 */
 	public function load_settings() {
 
-		if ( ! $this->settings = get_option( $this->settings_key ) ) {
-			return false;
-		}
+		if ( $this->current_version === '1.3.X' ) {
 
-		if ( !is_array( $this->settings ) ) {
-			return false;
+			if ( ! $this->settings = get_option( $this->settings_key ) ) {
+				return false;
+			}
+
+			if ( !is_array( $this->settings ) ) {
+				return false;
+			}
+
+		} else {
+
+			foreach ( $this->$setting_keys as $key ) {
+				$this->settings[ $key ] = get_option( $this->namespace_prefixed( $key ) );
+			}
+
 		}
 
 		return true;
@@ -41,7 +74,23 @@ abstract class SS_WC_MailChimp_Migration_Base {
 	 * @return [type] [description]
 	 */
 	public function save_settings() {
-		update_option( $this->settings_key , $this->settings );
+
+		if ( $this->target_version === '1.3.X' ) {
+
+			update_option( $this->settings_key , $this->settings );
+
+		} else {
+
+			foreach ( $this->settings as $key => $value ) {
+				update_option( $this->namespace_prefixed( $key ), $value );
+			}
+
+		}
+
+	}
+
+	private function namespace_prefixed( $suffix ) {
+		return $this->namespace . '_' . $suffix;
 	}
 
 }
