3cea66288b5e6315c0bbc67be874c17a472eeb56
Adam Anderly <adam@anderly.com>
2016-09-23 09:48:09 -0500

    Version bump and updated readme.

--- includes/class-ss-wc-mailchimp-plugin.php
+++ includes/class-ss-wc-mailchimp-plugin.php
@@ -9,7 +9,7 @@ final class SS_WC_MailChimp_Plugin {
 	 * Plugin version
 	 * @var string
 	 */
-	private static $version = '2.0.17';
+	private static $version = '2.0.18';
 
 	/**
 	 * Plugin singleton instance
