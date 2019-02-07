2a9b7b298087ffc816556c25755232fd4888c6cd
Adam Anderly <adam@anderly.com>
2019-02-07 12:03:56 -0600

    Change to get the latest WooCommerce release.

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -13,10 +13,26 @@ install_wc() {
 
   cd $INITIAL_DIR
 
-	if [ ! -d ../woocommerce ]; then
-		git clone https://github.com/woocommerce/woocommerce ../woocommerce
-		cd ../woocommerce
-		git checkout $WC_BRANCH
+	# if [ ! -d ../woocommerce ]; then
+	# 	git clone https://github.com/woocommerce/woocommerce ../woocommerce
+	# 	cd ../woocommerce
+	# 	git checkout $WC_BRANCH
+	# fi
+
+	# say "WooCommerce Installed"
+
+	# place a copy of woocommerce where the unit tests etc. expect it to be
+	mkdir -p "../woocommerce"
+	if [ $WC_VERSION == 'latest' ]; then
+		# Get the latest WooCommerce release
+		curl -L https://api.github.com/repos/woocommerce/woocommerce/releases/$WC_VERSION |
+			grep '"tarball_url":' | #Get tarball url line
+			sed -E 's/.*"([^"]+)".*/\1/' | #Get tarball url
+			xargs -I {} curl -sL "{}" | #Capture the argument and use to download the tarball
+			tar --strip-components=1 -zx -C "../woocommerce" #Extract
+	else
+		# Get the specified WooCommerce release
+		curl -sL https://api.github.com/repos/woocommerce/woocommerce/tarball/$WC_VERSION | tar --strip-components=1 -zx -C "../woocommerce"
 	fi
 
 	say "WooCommerce Installed"
